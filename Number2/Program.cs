5d3c3ee096b84a2fd1901b501d97fd3c715b76b1
Irina <ip_scheglova@mail.ru>
2022-11-25 10:21:55 +0500

    task about exit

---
+++ Number2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+
+string truePassword = "exit";
+
+
+while(true)
+{
+    string userPassword = ReadStr("Введите пароль: ");
+    if(userPassword==truePassword)
+    break;
+}
+
+string ReadStr(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
