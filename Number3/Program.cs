fa2dfe6dbb7e1a96dc7ef31c40478c81c8937a3b
Irina <ip_scheglova@mail.ru>
2022-11-25 21:30:04 +0500

    task about exit plus

---
+++ Number3/Program.cs
@@ -0,0 +1,14 @@
+﻿string truePassword = "exit";
+
+while(true)
+{
+    string userPassword = ReadStr("Введите пароль: ");
+    if(userPassword==truePassword)
+    break;
+}
+
+string ReadStr(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
