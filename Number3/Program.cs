0dd026db632d5a1efe12e5558f4f8be6ad303602
Irina <ip_scheglova@mail.ru>
2022-11-27 22:51:35 +0500

    add commands exit & help

--- Number3/Program.cs
+++ Number3/Program.cs
@@ -1,10 +1,28 @@
-﻿string truePassword = "exit";
+﻿string comExit = "exit";
+string comHelp = "help";
+string comNewAccount = "newaccount";
+string comSetUserName = "username";
+string comSetEmail = "email";
+string comSetPassword = "pasword";
+string comEnterAccount = "enter";
+string comWriteName = "writename";
 
-while(true)
+string userCommand = string.Empty;
+for( ; ;)
 {
-    string userPassword = ReadStr("Введите пароль: ");
-    if(userPassword==truePassword)
-    break;
+    userCommand = ReadStr("Введите команду: ");
+    if(userCommand.ToLower()==comExit) break;
+    if(userCommand.ToLower()==comHelp)
+    {
+        Console.WriteLine("exit");
+        Console.WriteLine("help");
+        Console.WriteLine("newaccount");
+        Console.WriteLine("username");
+        Console.WriteLine("email");
+        Console.WriteLine("pasword");
+        Console.WriteLine("enter");
+        Console.WriteLine("writename");
+    }
 }
 
 string ReadStr(string msg)
